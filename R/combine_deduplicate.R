37c581276ae66f2891b9946fd8b2fa65b73036ec
Barbara Vreede <b.vreede@gmail.com>
2019-12-03 13:37:26 +0100

    added deduplication function

--- R/combine_deduplicate.R
+++ R/combine_deduplicate.R
@@ -14,7 +14,38 @@ library(ggplot2)
 infiles = list.files(path = "./output", pattern="*_clean.csv")
 infiles = paste0("output/",infiles)
 myfiles = lapply(infiles, read_csv)
+# add source filename to myfiles
+for(i in 1:length(infiles)){
+  myfiles[[i]] <- myfiles[[i]] %>% mutate(source = infiles[i])
+}
 # bind all together to one large dataframe
 df <- bind_rows(myfiles)
 
-## Note that pure_id is system_id in the new input files!
+########################################################################
+##### NB!! Note that pure_id is system_id in the new input files! ######
+########################################################################
+
+
+deduplicate <- function(df){
+  #' Deduplication of the publication entries in a dataframe.
+  #' Required to use before reporting on a subset of publications, so that no bias is created
+  #' in the results if a publication is entered multiple times by different groups, eg.
+  # first determine whether there is a mix of multiple source files
+  sourcefiles <- df$source %>% as.factor() %>% levels()
+  if(length(sourcefiles) < 2){ # in this case, a single source file has been used, so deduplication can be performed on system ID
+    df <- distinct(df,system_id,.keep_all = T)   
+  }
+  else{
+    # separate between entries with and without doi
+    # deduplicate entries in the doi dataframe first
+    df_nondoi <- df %>% filter(is.na(doi))
+    df_doi <- df %>%
+      filter(df,!is.na(doi)) %>%
+      distinct(doi,.keep_all = T)
+    # combine with nondoi and deduplicate by title
+    df <- full_join(df_doi,df_nondoi) %>% 
+      mutate(title_lower = str_to_lower(title)) %>%
+      distinct(title_lower,.keep_all = T) %>%
+      select(-title_lower)
+  }
+}
