2273b9fbcd2ce3a70a8d926136ff0f1a909135c7
Barbara Vreede <b.vreede@gmail.com>
2019-12-03 14:12:42 +0100

    added title adjustment functionality

--- R/combine_deduplicate.R
+++ R/combine_deduplicate.R
@@ -27,7 +27,10 @@ df <- bind_rows(myfiles)
 
 
 deduplicate <- function(df){
-  #' Deduplication of the publication entries in a dataframe.
+  #' Deduplication of publication entries.
+  #' 
+  #' Deduplication function that uses a single dataframe and deduplicates
+  #' based on DOI and then on title.
   #' Required to use before reporting on a subset of publications, so that no bias is created
   #' in the results if a publication is entered multiple times by different groups, eg.
   # first determine whether there is a mix of multiple source files
@@ -40,12 +43,15 @@ deduplicate <- function(df){
     # deduplicate entries in the doi dataframe first
     df_nondoi <- df %>% filter(is.na(doi))
     df_doi <- df %>%
-      filter(df,!is.na(doi)) %>%
+      filter(!is.na(doi)) %>%
       distinct(doi,.keep_all = T)
     # combine with nondoi and deduplicate by title
     df <- full_join(df_doi,df_nondoi) %>% 
-      mutate(title_lower = str_to_lower(title)) %>%
-      distinct(title_lower,.keep_all = T) %>%
-      select(-title_lower)
+      mutate(title_lower = str_to_lower(title),
+             title_lower = str_replace_all(title_lower,"[:punct:]+", ""),
+             title_lower = str_replace_all(title_lower,"[:blank:]+","")) %>%
+      distinct(title_lower,.keep_all = T) #%>%
+      #select(-title_lower)
   }
+  return(df)
 }
