0f1b6076fb5045f5c0940f13c720034f92c6aed1
Barbara Vreede <b.vreede@gmail.com>
2020-02-07 13:46:06 +0100

    added pipeline from clean_data

--- oa_pipeline.R
+++ oa_pipeline.R
@@ -1,5 +1,156 @@
 ## load packages and functions
-source("oa_utilities.R")
+library(tidyverse)
+library(stringr)
+library(readxl)
+library(jsonlite)
+library(httr)
+library(magrittr)
+
+source("R/clean_data.R")
+source("config/config.R")
+
+allfiles <- read_excel("config/config_pub_files.xlsx")
+
+for(fn in allfiles){
+  # extract file name
+  fn <- fn[allfiles$File_info=="Filename"]
+  fn_ext <- str_split(fn,"\\.")[[1]]
+  # skip filenames without extensions
+  # NB this automatically skips the first column
+  # if the column 
+  if(length(fn_ext) < 2){
+    if(!fn == allfiles[1,1]){
+      warning("The filename ", fn, " does not have an extension. This file cannot be processed.\n")
+    }
+    next
+  }
+  # extract extension and put together filename
+  ext <- fn_ext[-1]
+  fn <- paste0("data/",fn)
+  # open the file
+  if(ext == "csv"){
+    df <- read_delim(fn, delim=";")
+  }
+  else if(ext=="tsv"){
+    df <- read_delim(fn,delim="\t")
+  }
+  df %>% head()
+}
+  # rename
+  colnames(pub_data)[colnames(pub_data) == id_column] <- "system_id"
+  colnames(pub_data)[colnames(pub_data) == title_column] <- "title"
+  colnames(pub_data)[colnames(pub_data) == issn_column] <- "issn"
+  colnames(pub_data)[colnames(pub_data) == doi_column] <- "doi"
+  
+  colnames(doaj)[colnames(doaj) == issn_column_doaj] <- "issn"
+  colnames(doaj)[colnames(doaj) == eissn_column_doaj] <- "eissn"
+  # classify
+  # save
+}
+
+
+
+#### LOAD AND CLEAN DATA ####
+
+## Classification data
+doaj <- read_excel(path_doaj)
+vsnu <- read_csv(path_vsnu)
+
+## Renaming columns so they will not have to be adjusted every time we run the script - should be in config file!
+
+
+## Adjust data types
+
+# Set ID as character, so that it will not be treated as a numeral
+pub_data$system_id %<>% as.character
+
+# Set ISSN and OA status as factors because they are fixed variables which we want to analyze.
+pub_data$issn %<>% as.factor
+
+## Clean data
+# clean DOI and ISSN, remove spaces and hyperlinks, change uppercase to lowercase etc.
+pub_data$issn <- clean_issn(pub_data$issn)
+pub_data$doi <- clean_doi(pub_data$doi)
+
+doaj$issn <- clean_issn(doaj$issn) 
+doaj$eissn <- clean_issn(doaj$eissn)
+
+vsnu$DOI <- clean_doi(vsnu$DOI)
+
+#### OA LABELLING ####
+## Collect information that can later be used for the classification pipeline.
+
+## Step 1: DOAJ ISSN matching
+doaj_issn <- union(doaj$issn[!is.na(doaj$issn)], # all DOAJ ISSN numbers from print, without NAs
+                   doaj$eissn[!is.na(doaj$eissn)]) # all DOAJ E-ISSN numbers, without NAs
+
+pub_data$DOAJ_ISSN_match <- pub_data$issn%in%doaj_issn
+
+## Step 2: VSNU DOI matching
+vsnu_doi <- vsnu$DOI[!is.na(vsnu$DOI)]
+
+pub_data$VSNU_doi_match <- pub_data$doi%in%vsnu_doi
+
+## Step 3: Unpaywall
+api_csv <- "csv" #indicate here whether you want to load existing data or use the UPW api
+
+# generate a database with unpaywall data using their REST API
+# use all DOIs as input
+alldois <- pub_data$doi[!is.na(pub_data$doi)] # all pub_data dois without NA
+
+# mine unpaywall API for each DOI
+outlist <- list()
+
+for(i in seq_along(alldois)){
+  doi <- alldois[i]
+  if(api_csv=="api"){ # only mine the api if the user wants to renew unpaywall data
+    res <- upw_api(doi)
+    outlist[[i]] <- res
+  }
+}
+
+if(api_csv=="api"){
+  unpaywall <- bind_rows(outlist)
+  # save unpaywall data
+  today <- as.character(Sys.Date())
+  upwname <- paste0("output/unpaywall_",today,".csv")
+  write_csv(unpaywall,upwname)
+} else if(api_csv=="csv"){
+  unpaywall <- read_csv(path_unpaywall)
+}
+
+# ensure unpaywall data is saved as factor
+unpaywall$evidence %<>% as.factor
+unpaywall$free_fulltext_url %<>% as.factor
+unpaywall$license %<>% as.factor
+unpaywall$oa_color %<>% as.factor
+
+# merge publication data with unpaywall
+## CONSIDER JOIN IN UPDATE ##
+pub_data_merge <- left_join(pub_data,unpaywall,by="doi")
+
+#### CLASSIFICATION PIPELINE ####
+## apply the classification function
+pub_data_merge <- mutate(pub_data_merge,
+                         OA_label=mapply(define_oa,
+                                         DOAJ_ISSN_match,
+                                         VSNU_doi_match,
+                                         oa_color))
+
+pub_data_merge <- mutate(pub_data_merge,
+                         OA_label_detail=mapply(define_oa_detailed,
+                                                DOAJ_ISSN_match,
+                                                VSNU_doi_match,
+                                                oa_color))
+
+## turn the results into factors
+pub_data_merge$OA_label %<>% as.factor
+pub_data_merge$OA_label_detail %<>% as.factor
+
+
+
+
+==============
 
 ## source file paths
 #path_pure_uu <- "data/UU-Monitoring_OA-2018-basislijst-report_3119.xls"
