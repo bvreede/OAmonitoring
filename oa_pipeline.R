80816c9fd588eb74df59a0d8ba1e9d3aaae3b80b
Barbara Vreede <b.vreede@gmail.com>
2020-02-28 13:03:52 +0100

    removed more zombie code

--- oa_pipeline.R
+++ oa_pipeline.R
@@ -37,7 +37,7 @@ for(col in allfiles){
   # skip filenames without extensions
   # NB this automatically skips the first column with row names
   if(length(fn_ext) < 2){
-    if(!fn == allfiles[1,1]){ # this is acceptable with the header.
+    if(!fn == allfiles[1,1]){ # this behavior is acceptable with the header.
       warning("The filename ", fn, " does not have an extension. This file cannot be processed.\n")
     }
     next
@@ -56,6 +56,7 @@ rm(alldata)
 
 
 # STEP TWO: APPLY CLASSIFICATION
+# NB: all data collected here is automatically saved in data/clean
 
 source("R/classification.R")
 # get data from VSNU, DOAJ, UPW
@@ -66,7 +67,15 @@ upwdf <- upw_pipeline(df)
 # perform the classification
 df <- classify_oa(df)
 
-# 
+
+
+# STEP THREE: REPORT
+
+
+
+
+
+
 
 
 
@@ -78,55 +87,8 @@ pure_manual <- mutate(pure_manual,manual=substring(handmatig,1,1)) %>%
   filter(manual=="A"|manual=="B"|manual=="C"|manual=="D")
 
 
-
-
-
-
-
-
-## Step 4: Unpaywall
-api_csv <- "csv" #indicate here whether you want to load existing data or use the UPW api
-
-# generate a database with unpaywall data using their REST API
-# use all DOIs as input
-alldois <- union(pure_uu$doi[!is.na(pure_uu$doi)], # all pure_uu dois without NA
-                 pure_umcu$doi[!is.na(pure_umcu$doi)]) # all UMCU dois without NA
-
-# mine unpaywall API for each DOI
-outlist <- list()
-
-
-# ensure unpaywall data is saved as factor
-unpaywall$evidence %<>% as.factor
-unpaywall$free_fulltext_url %<>% as.factor
-unpaywall$license %<>% as.factor
-unpaywall$oa_color %<>% as.factor
-
-# merge pure with unpaywall
-## CONSIDER JOIN IN UPDATE ##
-uu_merge <- left_join(pure_uu,unpaywall,by="doi")
-umcu_merge <- left_join(pure_umcu,unpaywall,by="doi")
-
-
-
-
-
-
-
-
 # Make a field that indicates whether information is available.
 # Information is available when there is a DOI, or when there is a confirmed VSNU ISSN.
 # Or when OA_STATUS is green, this is likely from Pure info; also counts as available info
 all_pubs <- mutate(all_pubs,
                    information = OA_label!="CLOSED"|is.na(electronic_version)|!is.na(doi_resolver))
-
-# Label duplicates should be done per unique collection. 
-# If possible it should use DOIs, but it might be that titles cover more ground.
-# For this purpose:
-# make a title field that does not have punctuation and uses lowercase only
-all_pubs <- mutate(all_pubs,
-                   title_lower = str_to_lower(str_replace_all(title.x, "[[:punct:]]", "")))
-
-
-
-
