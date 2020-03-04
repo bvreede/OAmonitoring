ec40fa9607965b4908720add119243461b702c7c
Barbara Vreede <b.vreede@gmail.com>
2020-03-04 12:44:45 +0100

    Added option to use saved data

--- config/config.r
+++ config/config.r
@@ -1,5 +1,6 @@
 # VSNU
-path_vsnu <- "data/mockdata_VSNUdeal.xlsx"
+#path_vsnu <- "data/mockdata_VSNUdeal.xlsx"
+path_vsnu <- "data/VSNU_2019_only.xlsx"
 
 #Fill in a valid email for use in the Unpaywall API
 email_address <- "b.m.i.vreede@uu.nl" 
@@ -9,6 +10,12 @@ cutoff_missing <- 0.05
 # What year is the report for?
 report_year <- 2019
 
+# For Unpaywall and the DOAJ:
+# Do you want to re-run API or use saved data?
+# Be sure when using saved data that this was acquired using the
+# same input data as you now want to use!
+use_doaj <- "saved" # "saved" or "api"
+use_upw <- "saved" # "saved" or "api"
 
-# CORE
-core_key <- "jHVeD2i6BotaTFvdgun3qbXUAM0z4Owy"
+path_doaj <- "data/clean/doaj_from_issn_2020-03-03.csv"
+path_upw <- "data/clean/upw_from_doi_2020-03-03.csv"
